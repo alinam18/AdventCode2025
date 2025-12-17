212ef7e54ee60e30d8fcd05976cfbfc05153135f
alinam18 <z5416104@ad.unsw.edu.au>
2025-12-17 13:42:36 +1100

    finished pt2 day 5

--- day_5/day_5_pt2.py
+++ day_5/day_5_pt2.py
@@ -4,6 +4,7 @@ import sys
 def main():
     # try:
     open_file = open("day_5_input.txt", "r")
+    # open_file = open("day_5_test.txt", "r")
     lines = open_file.readlines()
 
 
@@ -12,30 +13,64 @@ def main():
     check_mode = False
     count = 0
     check_range = []
-
+    ranges = []
+    end = False
+    # list them and sort them 
     for line in lines:
+        # clean
+        if line == '\n':
+            end = True
+            break
         line = line.strip()
-        
-        
-        
-        parts = line.split('-')
-        start = int(parts[0])
-        end = int(parts[1])
-        valid_ranges.append((start, end))
-        for one, two in check_range:
-            # skip will not put in check_range
-            if one <= start <= two and one <= end <= two:
-                continue
-            # end is greater then two
-            elif one <= start <= two and one <= end > two:
-                # change the range of two into end
-                check_range[one, end]
-            elif one >= start <= two and one <= end <= two:
-                check_range[start, two]
-            
-        if found:
-            count += 1
+        digit =  line.split("-")
+        # print(digit)
+        # line.digit[0]
+        ranges.append([int(digit[0]), int(digit[1])])
 
-    print(f"There are {count} available ingredients.")
+    ranges.sort(key=lambda x: int(x[0]))
+    print(ranges)
+    # next now we will compare the second digit only 
+    new_range = []
+    for index, numbers in enumerate(ranges):
+        print(f"checking {numbers} | {new_range}")
+        comparing_value_1 = numbers[0]
+        comparing_value_2 = numbers[1]
+        # print(comparing_value)
+        if not new_range:
+            new_range.append(numbers)
+            continue
+        else:
+            add = False
+            for n_index, n_numbers in enumerate(new_range):
+                # if curr_one <= two and one <= curr_two:
+                if n_numbers[0] <= comparing_value_2 and comparing_value_1 <= n_numbers[1]: 
+                    add = True
+                    print(f"overlapping n_numbers:{n_numbers} | comparing_value_1:{comparing_value_1} | comparing_value_2:{comparing_value_2}")
+                    new_start = min(n_numbers[0], comparing_value_1)
+                    new_end = max(n_numbers[1], comparing_value_2)
+                    new_range[n_index] = [new_start, new_end]
+                    # print(f"{new_range[n_index]}")
+            
+
+
+            if add == False:
+                new_range.append(numbers)
+
+    # now calculate
+    amount = 0
+    for numbers in new_range:
+        end = numbers[1]
+        start = numbers[0]
+        amount += (end - start) + 1
+        # print(amount)
+
+    # print()
+    print(f"Final {amount}: {new_range}")
+    # Final 344813017450467
+    print(f"Final {amount}")
+
+
+
+
 
 main()
